f15ed383aa9d3f00d813a25e3d4d8b60e51294bb
Alberto de Lucas Gómez <alberto5dlg@gmail.com>
2017-05-09 12:09:21 +0200

    Skeleton API REST

--- APIRest/controllers/oferta.js
+++ APIRest/controllers/oferta.js
@@ -1,5 +1,8 @@
+<<<<<<< 533092e3146b7ff5d267415e4a454a0063ee5322
 var db = require('../db/dbsql'); //Conexion a la BBDD 
 
+=======
+>>>>>>> Skeleton API REST
 exports.getDetails = function(pet, res) {
 	res.send("Detalles de Ofertas ...")
 }
--- APIRest/index.js
+++ APIRest/index.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+<<<<<<< 533092e3146b7ff5d267415e4a454a0063ee5322
 var app = express();
 
+=======
+//var db = require('./db');
+var app = express();
+>>>>>>> Skeleton API REST
 app.use(bodyParser.json());
 module.exports = app;
 
@@ -11,6 +16,10 @@ var peticiones = require('./routes/router');
 //Rutas de los metodos, segun clase
 app.use('/api', peticiones);
 
+<<<<<<< 533092e3146b7ff5d267415e4a454a0063ee5322
+=======
+
+>>>>>>> Skeleton API REST
 //Ruta generica del Server
 app.get('/',function(pet, res) { 
 	res.send("API REST MTIS");
@@ -23,7 +32,12 @@ app.listen(app.get('port'), function() {
   console.log('Aplicacion Node.js ejecutandose en el puerto: '+ app.get('port'));
 });
 
+<<<<<<< 533092e3146b7ff5d267415e4a454a0063ee5322
 
+=======
+//Conectamos con la Base de Datos
+//db.start();
+>>>>>>> Skeleton API REST
 
 
 
