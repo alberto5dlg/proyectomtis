6eb9a7014ebb1707fcf5db05c562998c01c65ba1
Alberto de Lucas Gómez <alberto5dlg@gmail.com>
2017-05-25 18:15:26 +0200

    api rest en proceso

--- APIRest/controllers/oferta.js
+++ APIRest/controllers/oferta.js
@@ -1,3 +1,5 @@
+var db = require('../db/dbsql'); //Conexion a la BBDD 
+
 exports.getDetails = function(pet, res) {
 	res.send("Detalles de Ofertas ...")
 }
--- APIRest/index.js
+++ APIRest/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-//var db = require('./db');
 var app = express();
+
 app.use(bodyParser.json());
 module.exports = app;
 
@@ -10,7 +10,6 @@ var peticiones = require('./routes/router');
 
 //Rutas de los metodos, segun clase
 app.use('/api', peticiones);
-
 
 //Ruta generica del Server
 app.get('/',function(pet, res) { 
@@ -24,8 +23,7 @@ app.listen(app.get('port'), function() {
   console.log('Aplicacion Node.js ejecutandose en el puerto: '+ app.get('port'));
 });
 
-//Conectamos con la Base de Datos
-//db.start();
+
 
 
 
