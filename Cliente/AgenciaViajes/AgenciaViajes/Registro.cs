43e70fd40da7390f9fa71b5ee65f229ae8eba36f
Alberto De Lucas Gómez <alberto5dlg@gmail.com>
2017-05-30 20:28:29 +0200

    añadido login añadido registro mostrar ofertas y contratarlas

--- Cliente/AgenciaViajes/AgenciaViajes/Registro.cs
+++ Cliente/AgenciaViajes/AgenciaViajes/Registro.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,33 @@ namespace AgenciaViajes
         public Registro()
         {
             InitializeComponent();
+        }
+
+        private void Registrate_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.Port = 3311;
+            builder.UserID = "root";
+            builder.Password = "root";
+            builder.Database = "proyectomtis";
+
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(builder.ToString());
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO usuario VALUES ('" + userBox.Text + "','" + passBox.Text + "','" + nombreBox.Text + "','" + apellidoBox.Text + "','" + emailBox.Text + "','" + telfBox.Text + "');";
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Registrado Correctamente");
+                this.Hide();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+                  
         }
     }
 }
